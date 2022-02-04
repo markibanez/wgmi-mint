cec56d7018a384b6af0790520715eedae4b48324
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-04 18:08:25 +0800

    Initial code

---
+++ whitelist-verification/server.js
@@ -0,0 +1,19 @@
+import {} from 'dotenv/config';
+import express from 'express';
+import { ethers } from 'ethers';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/get-signer-address', (req, res) => {
+    const { message, signature } = req.query;
+    const recoveredAddress = ethers.utils.verifyMessage(message, signature);
+    res.send({ recoveredAddress });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`WGMI Whitelist Verification listening on port ${process.env.PORT} on ${process.env.ENVIRONMENT}`);
+});
