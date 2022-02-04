6c1f140ff5009bd27aa8820672e83ada1cbe0b97
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-04 19:17:44 +0800

    Setup test UI

--- sample-client/src/App.js
+++ sample-client/src/App.js
@@ -2,24 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const connectWallet = () => {
+        alert('hello')
+    }
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <button onClick={connectWallet}>Connect Wallet</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
