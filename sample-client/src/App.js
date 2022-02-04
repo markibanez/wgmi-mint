af2aeffc602f4f624ddd5b78fe619bdd4006f50f
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-05 07:46:10 +0800

    Prototype commit

--- sample-client/src/App.js
+++ sample-client/src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
 
 function App() {
-    const connectWallet = () => {
-        alert('hello')
+    const apiBaseUri = 'http://localhost:7000';
+    const connectWallet = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum, 1);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
+        const message = 'Verifying for WGMI';
+        const signature = await signer.signMessage(message);
+
+        const response = await fetch(`${apiBaseUri}/whitelist-me?address=${address}&message=${message}&signature=${signature}`);
+        const json = await response.json();
+        console.log(json);
     }
 
     return (
--- whitelist-verification/server.js
+++ whitelist-verification/server.js
@@ -1,18 +1,32 @@
 import {} from 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { ethers } from 'ethers';
 
 const app = express();
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/get-signer-address', (req, res) => {
-    const { message, signature } = req.query;
+const alchemy = new ethers.providers.AlchemyProvider(1, process.env.ALCHEMY_API_KEY);
+app.get('/whitelist-me', async (req, res) => {
+    const { address, message, signature } = req.query;
+    console.log(address, message, signature);
     const recoveredAddress = ethers.utils.verifyMessage(message, signature);
-    res.send({ recoveredAddress });
-});
+
+    const validSignature = address !== recoveredAddress;
+
+    const erc721Abi = ['function balanceOf(address) view returns (uint)'];
+
+    const mmc = new ethers.Contract('0xB2E4e69527D57FA108c535721C057075a7a82E86', erc721Abi, alchemy);
+    const mmcBalance = await mmc.balanceOf(address);
+
+    res.send({ recoveredAddress, validSignature, mmcBalance });
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`WGMI Whitelist Verification listening on port ${process.env.PORT} on ${process.env.ENVIRONMENT}`);
